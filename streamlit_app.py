d4d9d16cf620a0459581e0e9ff270ec26367e77c
zw-codex <168478572+zw-codex@users.noreply.github.com>
2024-04-29 15:53:13 -0500

    Update streamlit_app.py
    'requests' library added

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
