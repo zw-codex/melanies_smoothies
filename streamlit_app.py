1e757ac049944ad33c9847fb2267922fcd67d9e4
zw-codex <168478572+zw-codex@users.noreply.github.com>
2024-04-29 19:56:30 -0500

    Update streamlit_app.py
    converted snowpark df to pandas df to enable LOC function

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,12 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
