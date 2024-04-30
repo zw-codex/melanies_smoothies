c571f19a84db50112485964ae087e194633b556b
zw-codex <168478572+zw-codex@users.noreply.github.com>
2024-04-29 19:51:05 -0500

    Update streamlit_app.py
    added search_on column and st.stop() to multiselect df

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
